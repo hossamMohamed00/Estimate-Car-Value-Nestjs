ccbfecac5dd9b2f4a69532bf0a3f9ceef2fe2a9b
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-01-30 15:43:41 +0200

    Refactor(Pipe, Middleware):Add Validation pipe and cookie-session middlware to app.module + make use of nest config service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,19 +7,52 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/reports.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+//! We use this approach because there is configuration mismatch between NEST and cookie-session
+const cookieSession = require('cookie-session');
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: 'db.sqlite',
+          // database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true
+        };
+      }
     }),
     UsersModule,
     ReportsModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    //? Add validation pipe
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  //? Add cookie-session as middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['cookieSessionString']
+        })
+      )
+      .forRoutes('*'); // Apply it to all requests (Global middleware)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-//! We use this approach because there is configuration mismatch between NEST and cookie-session
-const cookieSession = require('cookie-session');
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  //? Add validation pipe
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  );
-
-  //? Add cookie-session
-  app.use(
-    cookieSession({
-      keys: ['cookieSessionString']
-    })
-  );
 
   //? Setup Swagger
   const config = new DocumentBuilder()
