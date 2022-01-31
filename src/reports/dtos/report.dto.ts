b76f2353b2e51681e727e807e26e4dbec56ca9b0
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-01 01:29:40 +0200

    Use Serialize Interceptor to serialize the outgoing report entity(Add userId instead of entire user instance)

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,38 @@
+import { Expose, Transform } from 'class-transformer';
+/**
+ * Report dto that used to serialize the outgoing report data.
+ */
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  lng: number; //* Longitude
+
+  @Expose()
+  lat: number; //* Latitude
+
+  @Expose()
+  mileage: number;
+
+  /**
+   * ? Transform the entire user instance into single property called userId.
+   * ? obj --> is a reference to the original report entity
+   */
+
+  @Transform(({ obj }) => obj.user?.id)
+  @Expose()
+  userId: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Serialize } from 'src/Interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
+@Serialize(ReportDto)
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
