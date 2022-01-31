6382e430b5d401f7255d4be05000b95d817b3e6b
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-01 01:15:09 +0200

    Refactor report creation to assign user who created the report.

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 
@@ -13,7 +15,7 @@ export class ReportsController {
    */
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './reports.entity';
+import { User } from './../users/user.entity';
 
 @Injectable()
 export class ReportsService {
@@ -15,8 +16,11 @@ export class ReportsService {
    * @param reportDto - Report data
    * @returns report instance
    */
-  async create(reportDto: CreateReportDto) {
+  async create(reportDto: CreateReportDto, user: User) {
     const report = await this.repo.create(reportDto);
+
+    //? Assign user to report
+    report.user = user;
 
     return this.repo.save(report);
   }
