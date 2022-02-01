8845e351aaa10b5ddb9ddbce70c819ea27d5f83b
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-01 21:57:59 +0200

    Create get car value estimate endpoint + Create get-estimate DTO +Transforming Query String Data.

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -6,29 +6,30 @@ import {
   Max,
   Min
 } from 'class-validator';
-export class CreateReportDto {
+import { Transform } from 'class-transformer';
+
+export class GetEstimateDto {
   @IsString()
   make: string;
 
   @IsString()
   model: string;
 
-  @IsNumber()
-  @Min(0)
-  @Max(1000000)
-  price: number;
-
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2022)
   year: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number; //* Longitude
 
+  @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number; //* Latitude
 
+  @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards
 } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -15,11 +17,21 @@ import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Serialize(ReportDto)
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  /**
+   * Get estimate about car based on car details provided
+   * @query query - Dto of report
+   */
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
 
   /**
    * Create new report.
@@ -31,6 +43,12 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
 
+  /**
+   * Change report approved status
+   * @param id - Report id
+   * @param body - Report details
+   * @returns updated report
+   */
   @Patch('/:id')
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
